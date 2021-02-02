d7d00d997dcee39933e151f98761c50f173d457e
Medha <medhardixit@gmail.com>
2021-02-02 14:59:51 +0530

    second day of 100daysofcode

---
+++ patterns/numberpyramid.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    #endif
+
+    int n = 0;
+    cin >> n ;
+
+    for (int i = 1; i <= n; i++) // n number of rows
+    {
+        for (int j = 1; j <= i; j++) // each column contains  row number of columns      
+        {
+            cout << i;
+        }
+        
+        cout << endl; // after end of each row
+    }
+    
+     
+    return 0;
+}
+
